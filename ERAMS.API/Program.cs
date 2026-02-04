caa20ea86f12aeb4637d9a70ad7e88c801f13f2f
Aditya Parit <paritaditya089@gmail.com>
2026-02-04 19:17:53 +0530

    Configured JWT authentication, DI and initial database migration

--- ERAMS.API/Program.cs
+++ ERAMS.API/Program.cs
@@ -1,4 +1,14 @@
+
+using ERAMS.API.Data;
+using ERAMS.API.Helper;
+using ERAMS.API.Service;
+using ERAMS.API.ServiceImpl;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -6,6 +16,31 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"])
+            )
+        };
+    });
+
+builder.Services.AddDbContext<ContextClass>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DBCS")));
+
+builder.Services.AddScoped<IAuthService, AuthServiceImpl>();
+builder.Services.AddScoped<JwtTokenGenerate>();
+
 
 var app = builder.Build();
 
@@ -17,6 +52,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
